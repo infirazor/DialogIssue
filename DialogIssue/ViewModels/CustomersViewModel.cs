59e98bf0fa21eed750a981baa4605176a3f91ca4
Dhairya Joshi <dhairyajoshi21@gmail.com>
2018-11-19 08:51:58 +0530

    Final Code

---
+++ DialogIssue/ViewModels/CustomersViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Caliburn.Micro;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DialogIssue.ViewModels
+{
+    public class CustomersViewModel
+    {
+        public async Task AddNewCustomer()
+        {
+            var viewModel = new AddNewCustomerViewModel();
+            UIElement uiElement = ViewLocator.LocateForModel(viewModel, null, null);
+            ViewModelBinder.Bind(viewModel, uiElement, null);
+            await DialogHost.Show(uiElement);
+        }
+        public void Exit()
+        {
+            //Does not work
+            //(GetView() as Window).Close();
+
+            MessageBox.Show("Dont know how to close this windown other then Click Away of the Dialog");
+        }
+    }
+}
---
+++ DialogIssue/ViewModels/ShellViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Caliburn.Micro;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DialogIssue.ViewModels
+{
+    public class ShellViewModel
+    {
+        IWindowManager _manager;
+        public ShellViewModel()
+        {
+            _manager = new WindowManager();
+        }
+        public async Task OpenUserControl()
+        {
+            var viewModel = new AddNewCustomerViewModel();
+            UIElement uiElement = ViewLocator.LocateForModel(viewModel, null, null);
+            ViewModelBinder.Bind(viewModel, uiElement, null);
+            await DialogHost.Show(uiElement);
+        }
+        public async Task OpenWindow()
+        {
+            var viewModel = new CustomersViewModel();
+            UIElement uiElement = ViewLocator.LocateForModel(viewModel, null, null);
+            ViewModelBinder.Bind(viewModel, uiElement, null);
+            await DialogHost.Show(uiElement);
+        }
+        public void OpenCustomerWindowWithManager()
+        {
+            _manager.ShowDialog(new CustomersViewModel(), null, null);
+        }
+    }
+}
